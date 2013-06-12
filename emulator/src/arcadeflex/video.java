11a83f0e93076eeda65c38b12be8a64ca4b4c8a2
gmoralis <giorgosmrls@gmail.com>
2013-06-12 23:08:36 +0300

    added some implementation in generic video driver

---
+++ emulator/src/arcadeflex/video.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcadeflex;
+
+import static mame.osdependH.*;
+
+public class video {
+    public static osd_bitmap osd_new_bitmap(int width,int height,int depth) 
+    {
+        throw new UnsupportedOperationException("osd_new_bitmap");
+    }
+    public static void osd_free_bitmap(osd_bitmap bitmap)
+    {
+        throw new UnsupportedOperationException("osd_free_bitmap");
+    }
+}
--- emulator/src/vidhrdw/generic.java
+++ emulator/src/vidhrdw/generic.java
@@ -5,56 +5,64 @@ import static arcadeflex.libc_old.*;
 import static arcadeflex.osdepend.*;
 import static mame.osdependH.*;
 import static mame.driverH.*;
+import static mame.mame.*;
+import static arcadeflex.video.*;
 
 public class generic {
 
-   
     public static CharPtr videoram = new CharPtr();
     public static int[] videoram_size = new int[1];
     public static CharPtr colorram = new CharPtr();
     public static CharPtr spriteram = new CharPtr();	/* not used in this module... */
+
     public static CharPtr spriteram_2 = new CharPtr();
     public static CharPtr spriteram_3 = new CharPtr();
-/*TODO*///unsigned char *buffered_spriteram;	/* not used in this module... */
+    /*TODO*///unsigned char *buffered_spriteram;	/* not used in this module... */
 /*TODO*///unsigned char *buffered_spriteram_2;	/* ... */
     public static int[] spriteram_size = new int[1];/* ... here just for convenience */
+
     public static int[] spriteram_2_size = new int[1];/* ... here just for convenience */
+
     public static int[] spriteram_3_size = new int[1];/* ... here just for convenience */
-/*TODO*///unsigned char *flip_screen;	/* ... */
+    /*TODO*///unsigned char *flip_screen;	/* ... */
 /*TODO*///unsigned char *flip_screen_x;	/* ... */
 /*TODO*///unsigned char *flip_screen_y;	/* ... */
+
     public static char dirtybuffer[];
     static osd_bitmap tmpbitmap;
+    /**
+     * *************************************************************************
+     *
+     * Start the video hardware emulation.
+     *
+     **************************************************************************
+     */
+    public static VhStartPtr generic_vh_start = new VhStartPtr() {
+        public int handler() {
+            dirtybuffer = null;
+            tmpbitmap = null;
+            if (videoram_size[0] == 0) {
+                if (errorlog != null) {
+                    fprintf(errorlog, "Error: generic_vh_start() called but videoram_size not initialized\n");
+                }
+                return 1;
+            }
 
-    /***************************************************************************
+            if ((dirtybuffer = new char[videoram_size[0]]) == null) {
+                return 1;
+            }
 
-      Start the video hardware emulation.
+            memset(dirtybuffer, 1, videoram_size[0]);
 
-    ***************************************************************************/
-    public static VhStartPtr generic_vh_start = new VhStartPtr() { public int handler() 
-    {
-	dirtybuffer = null;
-	tmpbitmap = null;
-/*TODO*///
-/*TODO*///	if (videoram_size == 0)
-/*TODO*///	{
-/*TODO*///if (errorlog) fprintf(errorlog,"Error: generic_vh_start() called but videoram_size not initialized\n");
-/*TODO*///		return 1;
-/*TODO*///	}
-/*TODO*///
-/*TODO*///	if ((dirtybuffer = malloc(videoram_size)) == 0)
-/*TODO*///		return 1;
-/*TODO*///	memset(dirtybuffer,1,videoram_size);
-/*TODO*///
-/*TODO*///	if ((tmpbitmap = osd_new_bitmap(Machine->drv->screen_width,Machine->drv->screen_height,Machine->scrbitmap->depth)) == 0)
-/*TODO*///	{
-/*TODO*///		free(dirtybuffer);
-/*TODO*///		return 1;
-/*TODO*///	}
-/*TODO*///
+            if ((tmpbitmap = osd_new_bitmap(Machine.drv.screen_width, Machine.drv.screen_height, Machine.scrbitmap.depth)) == null) {
+                dirtybuffer = null;
+                return 1;
+            }
+
             return 0;
-    }};
-/*TODO*///
+        }
+    };
+    /*TODO*///
 /*TODO*///
 /*TODO*///int generic_bitmapped_vh_start(void)
 /*TODO*///{
@@ -73,15 +81,14 @@ public class generic {
 /*TODO*///
 /*TODO*///***************************************************************************/
 /*TODO*///void generic_vh_stop(void)
-    public static VhStopPtr generic_vh_stop = new VhStopPtr() { public void handler()
-    {
-/*TODO*///	free(dirtybuffer);
-/*TODO*///	osd_free_bitmap(tmpbitmap);
-/*TODO*///
-/*TODO*///	dirtybuffer = 0;
-/*TODO*///	tmpbitmap = 0;
-    }};
-/*TODO*///
+    public static VhStopPtr generic_vh_stop = new VhStopPtr() {
+        public void handler() {
+            dirtybuffer = null;
+            osd_free_bitmap(tmpbitmap);
+            tmpbitmap = null;
+        }
+    };
+    /*TODO*///
 /*TODO*///void generic_bitmapped_vh_stop(void)
 /*TODO*///{
 /*TODO*///	osd_free_bitmap(tmpbitmap);
@@ -102,34 +109,35 @@ public class generic {
 /*TODO*///		copybitmap(bitmap,tmpbitmap,0,0,0,0,&Machine->drv->visible_area,TRANSPARENCY_NONE,0);
 /*TODO*///}
 /*TODO*///
-    public static ReadHandlerPtr videoram_r = new ReadHandlerPtr() { public int handler(int offset)
-    {
-	return videoram.read(offset);
-    }};
+    public static ReadHandlerPtr videoram_r = new ReadHandlerPtr() {
+        public int handler(int offset) {
+            return videoram.read(offset);
+        }
+    };
+    public static ReadHandlerPtr colorram_r = new ReadHandlerPtr() {
+        public int handler(int offset) {
+            return colorram.read(offset);
+        }
+    };
+    public static WriteHandlerPtr videoram_w = new WriteHandlerPtr() {
+        public void handler(int offset, int data) {
+            if (videoram.read(offset) != data) {
+                dirtybuffer[offset] = 1;
 
-    public static ReadHandlerPtr colorram_r = new ReadHandlerPtr() { public int handler(int offset)
-    {
-	return colorram.read(offset);
-    } };   
-    public static WriteHandlerPtr videoram_w = new WriteHandlerPtr() { public void handler(int offset, int data)
-    {
-        if (videoram.read(offset) != data)
-	{
-            dirtybuffer[offset] = 1;
-		
-            videoram.write(offset, data);
-	}
-    }};  
-    public static WriteHandlerPtr colorram_w = new WriteHandlerPtr() { public void handler(int offset, int data)
-    {
-	if (colorram.read(offset) != data)
-	{
-            dirtybuffer[offset] = 1;
-		
-            colorram.write(offset, data);
-	}
-    }};    
-/*TODO*///int spriteram_r(int offset)
+                videoram.write(offset, data);
+            }
+        }
+    };
+    public static WriteHandlerPtr colorram_w = new WriteHandlerPtr() {
+        public void handler(int offset, int data) {
+            if (colorram.read(offset) != data) {
+                dirtybuffer[offset] = 1;
+
+                colorram.write(offset, data);
+            }
+        }
+    };
+    /*TODO*///int spriteram_r(int offset)
 /*TODO*///{
 /*TODO*///	return spriteram[offset];
 /*TODO*///}
