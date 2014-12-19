44b885d7867095700b2816e43d88a1a67da10dbe
gmoralis <george@moralis.gr>
2014-12-19 13:37:58 +0200

    improved dac emulation (not working correct yet)

--- emulator/src/sound/dac.java
+++ emulator/src/sound/dac.java
@@ -1,39 +1,37 @@
-
+/*
+ *  this file should be fully compatible for 0.36
+ */
 package sound;
 
-import mame.sndintrf;
-import mame.sndintrfH;
+import static arcadeflex.libc_old.*;
+import static arcadeflex.ptrlib.*;
+import mame.sndintrf.*;
 import static mame.sndintrfH.*;
 import static sound.dacH.*;
-import static mame.driverH.*;
+import static mame.mame.*;
 import static sound.streams.*;
+import static mame.driverH.*;
 
-public class dac extends sndintrf.snd_interface
-{
-    static int[] channel=new int[MAX_DAC];
-    static int[] output=new int[MAX_DAC];
-    static int[] UnsignedVolTable=new int[256];
-    static int[] SignedVolTable=new int[256];
-    
-    public dac()
-    {
-        sound_num=SOUND_DAC;
-        name="DAC";
+public class dac extends snd_interface {
+
+    static int[] channel = new int[MAX_DAC];
+    static int[] output = new int[MAX_DAC];
+    static int[] UnsignedVolTable = new int[256];
+    static int[] SignedVolTable = new int[256];
+
+    public dac() {
+        sound_num = SOUND_DAC;
+        name = "DAC";
     }
+
     @Override
     public int chips_num(MachineSound msound) {
-        return ((DACinterface)msound.sound_interface).num;
+        return ((DACinterface) msound.sound_interface).num;
     }
 
     @Override
     public int chips_clock(MachineSound msound) {
         return 0;//NO FUNCTIONAL CODE IS NECCESARY
-    }
-
-    @Override
-    public int start(MachineSound msound) {
-        return 0;
-//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -50,106 +48,99 @@ public class dac extends sndintrf.snd_interface
     public void reset() {
         //NO FUNCTIONAL CODE IS NECCESARY
     }
-    /*
 
-
-
-    static void DAC_update(int num,INT16 *buffer,int length)
-    {
+    public static StreamInitPtr DAC_update = new StreamInitPtr() {
+        public void handler(int num, UShortPtr buffer, int length) {
             int out = output[num];
+            int bi = 0;
+            while (length-- != 0) {
+                buffer.write(bi++, (char) out);
+            }
+        }
+    };
 
-            while (length--) *(buffer++) = out;
-    }
-*/
-   public static WriteHandlerPtr DAC_data_w = new WriteHandlerPtr() { public void handler(int num, int data)
-    {
+    public static WriteHandlerPtr DAC_data_w = new WriteHandlerPtr() {
+        public void handler(int num, int data) {
             int out = UnsignedVolTable[data];
 
-            if (output[num] != out)
-            {
-                    /* update the output buffer before changing the registers */
-                    stream_update(channel[num],0);
-                    output[num] = out;
+            if (output[num] != out) {
+                /* update the output buffer before changing the registers */
+                stream_update(channel[num], 0);
+                output[num] = out;
             }
-    }};
+        }
+    };
 
-   /*
-    void DAC_signed_data_w(int num,int data)
-    {
+    public static WriteHandlerPtr DAC_signed_data_w = new WriteHandlerPtr() {
+        public void handler(int num, int data) {
             int out = SignedVolTable[data];
 
-            if (output[num] != out)
-            {
-                    /* update the output buffer before changing the registers */
-   /*                 stream_update(channel[num],0);
-                    output[num] = out;
+            if (output[num] != out) {
+                /* update the output buffer before changing the registers */
+                stream_update(channel[num], 0);
+                output[num] = out;
             }
-    }
+        }
+    };
 
-
-    void DAC_data_16_w(int num,int data)
-    {
+    public static WriteHandlerPtr DAC_data_16_w = new WriteHandlerPtr() {
+        public void handler(int num, int data) {
             int out = data >> 1;		/* range      0..32767 */
 
-  /*          if (output[num] != out)
-            {
-                    /* update the output buffer before changing the registers */
-   /*                 stream_update(channel[num],0);
-                    output[num] = out;
+            if (output[num] != out) {
+                /* update the output buffer before changing the registers */
+                stream_update(channel[num], 0);
+                output[num] = out;
             }
-    }
+        }
+    };
 
-
-    void DAC_signed_data_16_w(int num,int data)
-    {
+    public static WriteHandlerPtr DAC_signed_data_16_w = new WriteHandlerPtr() {
+        public void handler(int num, int data) {
             int out = data - 0x8000;	/* range -32768..32767 */
 
-   /*         if (output[num] != out)
-            {
-                    /* update the output buffer before changing the registers */
-    /*                stream_update(channel[num],0);
-                    output[num] = out;
+            if (output[num] != out) {
+                /* update the output buffer before changing the registers */
+                stream_update(channel[num], 0);
+                output[num] = out;
             }
+        }
+    };
+
+    static void DAC_build_voltable() {
+        int i;
+
+        /* build volume table (linear) */
+        for (i = 0; i < 256; i++) {
+            UnsignedVolTable[i] = i * 0x101 / 2;	/* range      0..32767 */
+
+            SignedVolTable[i] = i * 0x101 - 0x8000;	/* range -32768..32767 */
+
+        }
     }
 
+    @Override
+    public int start(MachineSound msound) {
+        int i;
 
-    static void DAC_build_voltable(void)
-    {
-            int i;
+        DACinterface intf = ((DACinterface) msound.sound_interface);
 
+        DAC_build_voltable();
 
-            /* build volume table (linear) */
-     /*       for (i = 0;i < 256;i++)
-            {
-                    UnsignedVolTable[i] = i * 0x101 / 2;	/* range      0..32767 */
-     /*               SignedVolTable[i] = i * 0x101 - 0x8000;	/* range -32768..32767 */
-    /*        }
-    }
+        for (i = 0; i < intf.num; i++) {
+            String name;
 
+            name = sprintf("DAC #%d", i);
+            channel[i] = stream_init(name, intf.mixing_level[i], Machine.sample_rate, i, DAC_update);
 
-    int DAC_sh_start(const struct MachineSound *msound)
-    {
-            int i;
-            const struct DACinterface *intf = msound->sound_interface;
-
-
-            DAC_build_voltable();
-
-            for (i = 0;i < intf->num;i++)
-            {
-                    char name[40];
-
-
-                    sprintf(name,"DAC #%d",i);
-                    channel[i] = stream_init(name,intf->mixing_level[i],Machine->sample_rate,
-                                    i,DAC_update);
-
-                    if (channel[i] == -1)
-                            return 1;
-
-                    output[i] = 0;
+            if (channel[i] == -1) {
+                return 1;
             }
 
-            return 0;
-    }*/
+            output[i] = 0;
+        }
+
+        return 0;
+    }
+
 }
