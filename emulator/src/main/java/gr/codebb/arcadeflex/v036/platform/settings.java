4ae879fdb669220c600f3668516aad6ae20ec9bf
georgemoralis <giorgosmrls@gmail.com>
2020-12-22 17:28:44 +0200

    update to version 0.36.14

--- emulator/src/main/java/gr/codebb/arcadeflex/v036/platform/settings.java
+++ emulator/src/main/java/gr/codebb/arcadeflex/v036/platform/settings.java
@@ -5,6 +5,6 @@ package gr.codebb.arcadeflex.v036.platform;
  * @author shadow
  */
 public class settings {
-    public static String version = "arcadeflex v0.36.13";
+    public static String version = "arcadeflex v0.36.14";
     public static String romUrl = "http://www.jnodes.net/roms/";
 }
