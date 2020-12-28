ebdfddc07264f688a9b88921cec1cefd08ead68e
chusogar <chusogar@gmail.com>
2020-12-28 11:07:09 +0100

    download roms from Internet Archive

--- emulator/src/main/java/gr/codebb/arcadeflex/v036/platform/settings.java
+++ emulator/src/main/java/gr/codebb/arcadeflex/v036/platform/settings.java
@@ -6,5 +6,5 @@ package gr.codebb.arcadeflex.v036.platform;
  */
 public class settings {
     public static String version = "arcadeflex v0.36.14";
-    public static String romUrl = "http://archive.org/download/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/roms/";
+    public static String romUrl = "https://archive.org/details/MAME0.37b5_MAME2000_Reference_Set_Update_2_ROMs_Samples/roms/";
 }
